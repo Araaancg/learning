7adb1987a2e7c4db306cacdb6c1195bfda7bb0da
cice <carpinteroaranchag@gmail.com>
2021-11-22 22:01:29 +0100

    agregado lo de la fecha

--- weather/main.py
+++ weather/main.py
@@ -19,12 +19,33 @@ user = "0"
 while user.lower() != "q":
     wf.menu()
     user = input(": ")
-    if user == "1":
-        user = input("Introduzca el nombre de una ciudad: ").lower()
-        info_city_to_search = wf.search_by_city(user)
-        print("-"*50)
-        wf.pretty_print(user,info_city_to_search)
-        print("-"*50)
-        user = input("Presione enter para volver al menu principal ")
+    if user in options:
+        if user == "1":
+            user = input("Introduzca el nombre de una ciudad: ").lower()
+            # info_city = wf.search_by_city(user)
+            info_city = wf.get_forcast(user,city=True)
+            print("-"*50)
+            wf.pretty_print(user,info_city)
+            print("-"*50)
+            user = input("Presione enter para volver al menu principal ")
+        if user == "2":
+            lattlong = input("Introduzca la latitud y la longitud deseadadeseada (latt,long): ")
+            info_city = wf.get_forcast(lattlong, coordinates=True)[0]
+            city_name = wf.get_forcast(lattlong, coordinates=True)[1]     
+            print("-"*50)
+            wf.pretty_print(city_name, info_city)
+            print("-"*50)
+            user = input("Presione enter para volver al menú principal ")
+        if user == "3":
+            user = input("Introduzca el nombre de una ciudad: ").lower()
+            print("A continuación introduzca la fecha")
+            year = input("Año: ")
+            month = input("Mes: ")
+            day = input("Day: ")
+            print("-"*50)
+            info_city = wf.get_forcast(user,date=(year,month,day))
+            wf.pretty_print_date(user,info_city)
+    else:
+        print("Input inválido, por favor introduzca de nuevo la opción deseada")
         
 print("Hasta pronto!")
--- weather/weather_functions.py
+++ weather/weather_functions.py
@@ -16,23 +16,61 @@ def menu():
     print("EL TIEMPO".center(50))
     print("1. Buscar por ciudad")
     print("2. Buscar por coordendadas (latitud,longitud)")
-    # print("3. Buscar el tiempo de una ciudad por fecha")
+    print("3. Buscar el tiempo de una ciudad por fecha")
     print("Q. Terminar programa")
     print("-"*50)
 
-def search_by_city(city):
-    res = req.get(f"https://www.metaweather.com/api/location/search/?query={city}").json()
-    woeid = res[0]["woeid"]
-    res = req.get(f"https://www.metaweather.com/api/location/{woeid}").json()
-    return res
-    # print(woeid)
-# search_by_city("madrid")
+# def search_by_city(city):
+#     res = req.get(f"https://www.metaweather.com/api/location/search/?query={city}").json()
+#     woeid = res[0]["woeid"]
+#     res = req.get(f"https://www.metaweather.com/api/location/{woeid}").json()
+#     return res
+
+
+# def search_by_coordinates(latitude,longitude):
+#     res = req.get(f"https://www.metaweather.com/api/location/search/?lattlong={latitude},{longitude}").json()
+#     woeid = res[0]["woeid"]
+#     city_name = res[0]["title"]
+#     res = req.get(f"https://www.metaweather.com/api/location/{woeid}").json()
+#     return res, city_name
+
+def get_forcast(method,**kwargs):
+    if kwargs.get("date"):
+        # if kwargs.get("city"):
+        res = req.get(f"https://www.metaweather.com/api/location/search/?query={method}").json()
+        woeid = res[0]["woeid"]
+        res = req.get(f"https://www.metaweather.com/api/location/{woeid}/{kwargs.get('date')[0]}/{kwargs.get('date')[1]}/{kwargs.get('date')[2]}").json()
+        return res
+    else:
+        if kwargs.get("city"):
+            res = req.get(f"https://www.metaweather.com/api/location/search/?query={method}").json()
+            woeid = res[0]["woeid"]
+            # print(woeid)
+            res = req.get(f"https://www.metaweather.com/api/location/{woeid}").json()
+            return res
+        if kwargs.get("coordinates"):
+            res = req.get(f"https://www.metaweather.com/api/location/search/?lattlong={method}").json()
+            woeid = res[0]["woeid"]
+            city_name = res[0]["title"]
+            res = req.get(f"https://www.metaweather.com/api/location/{woeid}").json()
+            return res, city_name
+
+# print(get_forcast("madrid",date=("2021","6","21")))
 
 def pretty_print(city_name, city_list):
     print(city_name.upper().center(45))
     print(f"Descripción del tiempo: {city_list['consolidated_weather'][0]['weather_state_name']}")
-    print(f"    Temperatura máxima: {city_list['consolidated_weather'][0]['max_temp']}")
-    print(f"    Temperatura mínima: {city_list['consolidated_weather'][0]['min_temp']}")
-    print(f"     Sensación térmica: {city_list['consolidated_weather'][0]['the_temp']}")
-    print(f"               Humedad: {city_list['consolidated_weather'][0]['humidity']}")
-    print(f"  Velocidad del viento: {city_list['consolidated_weather'][0]['wind_speed']}")
+    print(f"    Temperatura máxima: {city_list['consolidated_weather'][0]['max_temp']}°")
+    print(f"    Temperatura mínima: {city_list['consolidated_weather'][0]['min_temp']}°")
+    print(f"     Sensación térmica: {city_list['consolidated_weather'][0]['the_temp']}°")
+    print(f"               Humedad: {city_list['consolidated_weather'][0]['humidity']} %")
+    print(f"  Velocidad del viento: {city_list['consolidated_weather'][0]['wind_speed']} m/s")
+
+def pretty_print_date(city_name, city_list):
+    print(city_name.upper().center(45))
+    print(f"Descripción del tiempo: {city_list[0]['weather_state_name']}")
+    print(f"    Temperatura máxima: {city_list[0]['max_temp']}°")
+    print(f"    Temperatura mínima: {city_list[0]['min_temp']}°")
+    print(f"     Sensación térmica: {city_list[0]['the_temp']}°")
+    print(f"               Humedad: {city_list[0]['humidity']} %")
+    print(f"  Velocidad del viento: {city_list[0]['wind_speed']} m/s")

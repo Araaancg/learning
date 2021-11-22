1e048a8e623ca1ece695969b42799c8c0eed90de
Araaancg <carpinteroaranchag@gmail.com>
2021-11-22 12:06:40 +0100

    haciendo los ejercicios bien hechos

--- municipiosm/main.py
+++ municipiosm/main.py
@@ -2,109 +2,114 @@ import requests as req
 import csv
 import json
 
+'''
+Ejercicio municipios Madrid
+Al principio trabajamos con csv y al final convertiremos el archivo en un json
 
-# url = "https://datos.comunidad.madrid/catalogo/dataset/032474a0-bf11-4465-bb92-392052962866/resource/ee750429-1e05-411a-b026-a57ea452a34a/download/municipio_comunidad_madrid.csv"
-# res = req.get(url).content #de la url cogemos los datos, en este caso el csv
-# res = res.decode("utf-8", errors="replace")
-# print(res)
+Obtener:
+- El municipio por INE
+- El municipio más grande
+- Superficie total
+- Densidad total
+- Población total de Madrid
+- Población media de los municipios
+- Comprobar la ley de benford con la superficie
+- Convertir el csv en un json
+'''
 
-#decodificamos el csv porque viene en type bytes
+# Primero importamos la data de la pag web oficial de la c. de Madrid (en csv)
+
+url = "https://datos.comunidad.madrid/catalogo/dataset/032474a0-bf11-4465-bb92-392052962866/resource/ee750429-1e05-411a-b026-a57ea452a34a/download/municipio_comunidad_madrid.csv"
+res = req.get(url).content
+# La info esta codificada asi que la tenemos que decodificar. Las tildes y ñ's quedan como ?
+res = res.decode("utf-8",errors="replace")
+# El replace los reemplaza con la "?", el ignore los eliminaría pero perdemos info
 
 # with open("data_municipios.csv", mode="w", encoding="utf8") as file:
 #     file.write(res)
 
+# Ya tenemos el archivo escrito
+
+# EJERCICIO 1: Obtener el municipio por código INE
 def get_by_ine(dataset, ine_code):
-    for mun in dataset[1:]:
+    for mun in dataset[1:]: # El [1:] para que no coja la primera línea que son los headings
         if mun[2] == ine_code:
             return mun
 
-def bigger_mun(dataset):
-    big_area = 0
-    big_mun = None
+# EJERCICIO 2: Obtener el municipio más grande
+def get_biggest_mun(dataset):
+    biggest_area = 0
+    biggest_mun = None
     for mun in dataset[1:]:
-        if float(mun[-2]) > big_area:
-            big_area = float(mun[-2])
-            big_mun = mun
-    return big_mun
+        if float(mun[-2]) > biggest_area:
+            biggest_area = float(mun[-2])
+            biggest_mun = mun
+    return biggest_mun
 
-def sup_total(dataset):
-    superficie = 0
+# EJERCICIO 3: Obtener la superficie total
+def get_sup_total(dataset):
+    sup_total = 0
     for mun in dataset[1:]:
-        superficie += float(mun[-2])
-        # print(superficie)
-    return superficie
+        sup_total += float(mun[-2])
+    return sup_total
 
-def get_pobl_total(dataset):
-    pobl_total = 0
+# EJERCICIO 4: Obtener la densidad total
+def get_total_density(dataset):
+    total_density = 0
     for mun in dataset[1:]:
-        pobl_local = float(mun[-1])*float(mun[-2])
-        pobl_total += pobl_local
-    return pobl_total
+        total_density += float(mun[-1])
+    return total_density
 
+# EJERCICIO 5: Obtener la población total (densidad*superficie)
+def get_total_population(dataset):
+    total_population = 0
+    for mun in dataset[1:]:
+        local_population = float(mun[-2])*float(mun[-1])
+        total_population += local_population
+    return total_population
 
+# EJERCICIO 6: Población de media entre todos los municipios
+def get_average_population(dataset):
+    total_population = 0
+    for mun in dataset[1:]:
+        local_population = float(mun[-2])*float(mun[-1])
+        total_population += local_population
+    average_population = total_population/len(dataset[1:])
+    return average_population
 
-with open("./data_municipios.csv", mode="r", encoding="utf8") as file:
+# EJERCICIO 7: Comprobar la ley de benford
+def benford(dataset):
+    pass
+
+with open("data_municipios.csv", mode="r", encoding="utf8") as file:
     data = list(csv.reader(file, delimiter=";"))
-    del data[0]
-    
+    # lo convertimos en lista para que se pueda sacar de la indentación del open
 
-# #obtener municipio por ine
-# mun_to_search = get_by_ine(data, "28014")
-# print(mun_to_search)
+# Ejercicio 1
+mun_to_search = get_by_ine(data,"280014")
+print(f"EJERCICIO 1: {mun_to_search}")
 
-# #municipio más grande
-# mun_to_search = bigger_mun(data)
-# print(mun_to_search)
+# Ejercicio 2
+biggest_mun = get_biggest_mun(data)
+print(f"EJERCICIO 2: {biggest_mun}")
 
-# #Obtener superficie total
-# superficie_total = sup_total(data)
-# print(superficie_total)
+# Ejercicio 3
+sup_total = get_sup_total(data)
+print(f"EJERCICIO 3: {sup_total}")
 
-# #Obtener la poblacion de madrid
-# poblacion_madrid = get_pobl_total(data)
-# print(poblacion_madrid)
+#Ejercicio 4
+total_density = get_total_density(data)
+print(f"EJERCICIO 4: {total_density}")
 
-# #Obtener la población media de los municipios
-# poblacion_media = get_pobl_total(data) / len(data)
-# print(poblacion_media)
+#Ejercicio 5
+total_population = get_total_population(data)
+print(f"EJERCICIO 5: {total_population}")
 
-#Ley de benhorf
+#Ejercicio 6
+average_population = get_average_population(data)
+print(f"EJERCICIO 6: {average_population}")
 
 
-#Ejercicio 8. convertir el csv en json
-
-# función de conversión y luego le aplico el data set
-
-def csv_to_dict(data_base):
-    with open("data.csv", mode="r") as file:
-        csv_reader = csv.reader(file, delimiter=";")
-        next(csv_reader)
-        for i, row in enumerate(csv_reader):
-            # print(i, row)
-            book = {
-                "id": row[0],
-                "title": row[1],
-                "author":row[2],
-                "genre":row[3]
-            }
-            data_base.append(book)
-
-data_dict = []
-
-def csv_to_json(new_dict):
-    with open("data_municipios.csv", mode="r", encoding="utf8") as file:
-        csv_reader = csv.reader(file, delimiter=";")
-        next(csv_reader)
-        for i, row in enumerate(csv_reader):
-            mun = {
-                "municipio_codigo":row[0],
-                "municipio_nombre":row[1],
-                "municipio_codigo_ine":row[2],
-                "nuts4_nombre":row[3],
-                "superficie_km2":row[4],
-                "densidad_por_km2":row[5],
-            }
-            new_dict.append(mun)
-        json.dump(dict(new_dict),"data_municipios.json")
-
-csv_to_json(data_dict)
+'''
+Falta benhorf y la conversión de csv a json
+'''

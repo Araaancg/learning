dfb8ff248a6eeea91d21b3438f1e23f267986f6a
cice <carpinteroaranchag@gmail.com>
2021-12-02 21:54:45 +0100

    pokemon recibe ataque

--- pokemon/main.py
+++ pokemon/main.py
@@ -9,16 +9,37 @@ class Pokemon:
         self.HP = HP
         self.attacks = []
 
-    def learn(self, attack_name):
-        self.attacks.append(attack_name)
+    def learn(self, attack):
+        self.attacks.append(attack)
+
+    def receive_damage(self,attack):
+        self.HP -= attack.damage
 
     def __str__(self):
-        return f"Pokemon:({self.name},{self.element},{self.HP})"
+        return f"Pokemon({self.name},{self.element},{self.HP})"
    
+class Attack:
+    def __init__(self,name,element,damage):
+        self.name = name
+        self.element = element
+        self.damage = damage
+    
+    def __repr__(self):
+        return f"Attack({self.name},{self.element},{self.damage}"
 
-charmander = ["Charmander",elements[0],100.0]
-print(charmander)
 
-print(charmander.attacks)
-charmander.learn("fireball")
-print(charmander.attacks)
+
+charmander = Pokemon("Charmander",elements[0],100.0)
+squirtle = Pokemon("Squirtle",elements[1],110.0)
+bulbasur = Pokemon("Bulbasur",elements[2],95.0)
+
+fireball = Attack("Fireball",elements[0],25.0)
+
+charmander.learn(fireball)  # charmander aprende un ataque, definido en la calse attack
+print(squirtle.HP)              
+squirtle.receive_damage(charmander.attacks[0])  # squirtle recibe daño del ataque de charmander
+print(squirtle.HP)
+
+
+
+

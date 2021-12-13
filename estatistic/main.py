80d338cebd733338cc4086189f809fe19d94e205
cice <carpinteroaranchag@gmail.com>
2021-12-13 21:56:41 +0100

    nuevo proyecto estadística

---
+++ estatistic/main.py
@@ -0,0 +1,46 @@
+# Datos abiertos de la c. Madrid, incidencia covid
+import requests as req
+import json
+import obj_std as std
+import matplotlib.pyplot as pty
+
+url = "https://datos.comunidad.madrid/catalogo/dataset/b3d55e40-8263-4c0b-827d-2bb23b5e7bab/resource/907a2df0-2334-4ca7-aed6-0fa199c893ad/download/covid19_tia_zonas_basicas_salud_s.json"
+res = req.get(url).json()
+
+with open("./covid.json",mode="w",encoding="utf8") as file:
+    json.dump(res,file,indent=4,ensure_ascii=False)
+
+def get_data(json_file):
+    with open(json_file,encoding="utf8") as file:
+        return json.load(file)
+
+data = get_data("./covid.json")
+# print(data)
+
+def data_by_date(dataset):
+    result = {}
+    for zone in dataset["data"]:
+        idate = zone["fecha_informe"].split(" ")[0]
+        if result.get(idate):
+            result[idate].append(zone)
+        else:
+            result[idate] = [zone]
+    return result
+
+data_by_date = data_by_date(data)
+
+x = [num for num in range(1, len(data_by_date) + 1)]
+y = []
+for zones_by_date in data_by_date.values():
+    y.append(sum(z["casos_confirmados_totales"] for z in zones_by_date))
+y.reverse()
+print(y)
+
+analyze = std(x,y)
+
+
+
+
+
+
+

c47731c47254f1919adaea60034e402f1d68863e
Araaancg <carpinteroaranchag@gmail.com>
2021-12-14 16:55:49 +0100

    graficas y explicaciones

--- estatistic/main.py
+++ estatistic/main.py
@@ -1,8 +1,8 @@
 # Datos abiertos de la c. Madrid, incidencia covid
 import requests as req
 import json
-import obj_std as std
-import matplotlib.pyplot as pty
+from obj_std import *
+import matplotlib.pyplot as plt
 
 url = "https://datos.comunidad.madrid/catalogo/dataset/b3d55e40-8263-4c0b-827d-2bb23b5e7bab/resource/907a2df0-2334-4ca7-aed6-0fa199c893ad/download/covid19_tia_zonas_basicas_salud_s.json"
 res = req.get(url).json()
@@ -15,8 +15,9 @@ def get_data(json_file):
         return json.load(file)
 
 data = get_data("./covid.json")
-# print(data)
 
+
+# Función que convierte el data en un DICCIONARIO (keys: fechas, values: lista de info de cada municipio(dic))
 def data_by_date(dataset):
     result = {}
     for zone in dataset["data"]:
@@ -29,17 +30,24 @@ def data_by_date(dataset):
 
 data_by_date = data_by_date(data)
 
-x = [num for num in range(1, len(data_by_date) + 1)]
-y = []
-for zones_by_date in data_by_date.values():
-    y.append(sum(z["casos_confirmados_totales"] for z in zones_by_date))
-y.reverse()
-print(y)
 
-analyze = std(x,y)
+#CASOS CONFIRMADOS TOTALES
+casos_totales = sum([zone["casos_confirmados_totales"] for zone in data_by_date["2021/12/07"]])
+# print(casos_totales)
+# print(len(data_by_date["2021/12/07"]))
 
 
+# GRÁFICA
+x = [num for num in range(1, len(data_by_date) + 1)] # Representa las semanas (de la primera a la 80)
+y = [] 
+for zones_by_date in data_by_date.values(): #valores de y
+      y.append(sum(z["casos_confirmados_totales"] for z in zones_by_date))
+y.reverse() 
 
+
+analyze = Std(x,y)
+
+predicted_y = analyze.lineals
 
 
 
--- estatistic/obj_std.py
+++ estatistic/obj_std.py
@@ -1,67 +1,110 @@
-class std:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+'''
+Objeto estadística para poder analizar datos, en este caso los casos de covid confirmados en la c. Madrid
+Términos de estadística
+'''
 
-    @property    
+class Std:
+    def __init__(self, x, y):
+        self.x = x #Variable independiente
+        self.y = y #Variable dependiente
+
+    @property 
     def n(self):
         return len(self.x)
     
-    @property    
+
+
+    '''
+    MEDIA: La media es el valor promedio de un conjunto de datos numéricos, 
+    calculada como la suma del conjunto de valores dividida entre el número total de valores.
+    Ecuación: Σ(x)/n
+    '''
+    @property   
     def x_avg(self):
         return sum(self.x)/self.n
 
-    @property    
+    @property   
     def y_avg(self):
         return sum(self.y)/self.n
     
-    @property
-    def sigmaXY(self):
-        return sum((xi * yi for xi, yi in zip(self.x, self.y)))
-    
-    @property
-    def sigmaXsq(self):
-        return sum((xi**2 for xi in self.x))
 
-    @property
-    def sigmaYsq(self):
-        return sum((yi**2 for yi in self.y))
 
+    '''
+    VARIANZA: medida de dispersión
+    que se utiliza para representar la variabilidad de un conjunto de datos 
+    respecto de la media aritmética de los mismo.
+    Ecuación: Σi(xi-x_avg)^2/n
+    '''
     @property    
     def x_variance(self):
         de = sum([(xi - self.x_avg)**2 for xi in self.x])
         return de/self.n
     
     @property    
-    def y_variance(self):
+    def y_variance(self): # Se calcula con x porque es la variable independiente, la y viene de la x
         de = sum([(yi - self.y_avg)**2 for yi in self.y])
         return de/self.n
+    
 
+
+    '''
+    COVARIANZA: La covarianza es el valor que refleja en qué cuantía dos variables aleatorias varían 
+    de forma conjunta respecto a sus medias. 
+    Nos permite saber cómo se comporta una variable en función de lo que hace otra variable
+    Ecuación: Σi((xi-x_avg)(yi-y_avg))/n
+    '''
     @property
     def covariance(self):
-        first_term = self.sigmaXY/self.n
-        return first_term - self.x_avg * self.y_avg
+        x_list = [(xi-self.x_avg) for xi in self.x]
+        y_list = [(yi-self.y_avg) for yi in self.y]
+        nu = sum([tup[0]*tup[1] for tup in zip(x_list,y_list)])
+        return nu/self.n
     
+    
+
+    '''
+    COEFICIENTE DE CORRELACIÓN LINEAL (DE PEARSON): Es una medida estadística que cuantifica 
+    la dependencia lineal entre dos variables.
+    Ecuación: cov(x,y)/σ(x)*σ(y)
+    σ -> es el símbolo para la desviación típica, el cual se calcula haciendo la raíz
+    cuadrada a la varianza
+    '''
     @property
     def r(self):
         de = self.covariance
         nu = ((self.x_variance) ** 0.5) * ((self.y_variance) ** 0.5)
         return de/nu
 
+
+    '''
+    B -> PEDIENTE
+    Ecuación:  (nΣXY-ΣX*ΣY) / (nΣ(X^2)-(ΣX)^2)
+    '''
     @property
     def B(self):
-        de = self.n*self.sigmaXY - sum(self.x)*sum(self.y)
-        nu = self.n*self.sigmaXsq - sum(self.x)**2
+        de = self.n*sum([xi*yi for xi,yi in zip(self.x,self.y)]) - sum(self.x)*sum(self.y)
+        nu = self.n*sum([xi**2 for xi in self.x]) - (sum(self.x)**2)
         return de/nu
     
+    @property
     def B0(self):
         return self.y_avg - self.B*self.x_avg
     
+
+    '''
+    VALORES DE Y EN BASE A X:
+    Ecuación y' = B*x + B0
+    '''
     def y_prediction(self,x_value):
         return self.B * x_value + self.B0
+
+    @property
+    def lineals(self):
+        return [self.y_prediction(week) for week in self.x]
 
     def __str__(self):
         return f"x: {self.x }\ny: {self.y }\nn: {self.n}\n"
 
-aula_5 = std([1,2,3], [10,20,30])
+aula_5 = Std([1,2,3], [10,20,27])
 # print(aula_5)
+# print(aula_5.B)

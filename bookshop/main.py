63298c102dd704337e37e15ba938644c0f14007a
Araaancg <carpinteroaranchag@gmail.com>
2021-12-21 14:34:28 +0100

    ahora si que si terminado

--- bookshop/main.py
+++ bookshop/main.py
@@ -89,8 +89,9 @@ while user.lower() != "q":
                 print(" ")
                 user = "0"
                 bs.menu_remove()
+                print("-"*50)
                 while user != "3": #Terminar acción
-                    user = input(":" )
+                    user = input(": ")
                     if user == "2": #Elegir otro libro
                         user = input("Introduzca id: ")
                         book = bs.get_by_term("id", user)
@@ -99,38 +100,37 @@ while user.lower() != "q":
                         user = "0"
                         bs.menu_remove()
                     if user == "1": #Eliminar libro
-                        # print(book)
-                        # bs.history(book, "Removal") 
+                        bs.history(book, "Removal") 
                         bs.remove_book(book[0], db)
+                        print("-"*50)
+                        print("Libro eliminado correctamente")
+                        print("-"*50)
                         user = "3"
             else:
                 print("\nNo hemos encontrado ninguna coincidencia en nuestra base de datos\n")
             input("Presione enter para volver al menu")
 
         elif user == "7":
-            # new_book = {}
             new_book = [bs.add_book(db,genre_list)]
-            # print(new_book)
             bs.history(new_book, "Insertion")
             print(" ")
             bs.pretty_book(new_book)
             input("\nPresione enter para volver al menu")
 
         elif user == "8":
-            print("-"*50)
             bs.pretty_book(db)
             print("-"*50)
-            input("\nPresione enter para volver al menu")
+            input("Presione enter para volver al menu")
 
-        # elif user.lower() == "q":             # Mensaje final programa y guardar los cambios
-            # user = input("¿Desea guardar los cambios? (y/n): ").lower()
-            # while user != "n":    
-            #     if user == "y":
-            #         bs.export_db(db, "data.csv")
-            #         print("Cambios guardados")
-            #         user = "n"
-            #     else:
-            #         user = input("Opción inexistente, por favor elija de nuevo (y/n): ")
+        elif user.lower() == "q":   # Mensaje final programa y guardar los cambios
+            user = input("¿Desea guardar los cambios? (y/n): ").lower()
+            while user != "n":    
+                if user == "y":
+                    bs.export_db(db, "data.csv")
+                    print("Cambios guardados")
+                    user = "n"
+                else:
+                    user = input("Opción inexistente, por favor elija de nuevo (y/n): ")
             
             print("¡Hasta pronto!")
             user = "q"

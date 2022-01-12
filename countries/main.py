a945ae21627571651ba980cc5467f9e6ad999f63
Araaancg <carpinteroaranchag@gmail.com>
2022-01-12 18:04:48 +0100

    terminado ignorando keyError capital

--- countries/main.py
+++ countries/main.py
@@ -1,4 +1,5 @@
 import requests as req
+import json
 
 '''
 APP DE TERMINAL
@@ -12,26 +13,27 @@ características
 
 import c_functions as cf
 
+separador = "-"*30
 
 user = "0"
 
 while user != "q".lower():
     cf.menu()
     user = input(": ")
+    print(separador)
 
     if user == "1":
-        user = input("Introduzca el país: ").lower()
+        user = input("Name of the country: ").lower()
         country_to_search = cf.get_by_term(user)
-        print("-"*30)
+        print(separador)
         if country_to_search:
             cf.pretty_print(country_to_search)
         else:
-            print(f"El país {user.capitalize()} no existe en nuestra base de datos")
-        print("-"*30)
-        input("Presione enter para volver al menú principal ")
+            print(f"The country {user.capitalize()} does not exist in our data base")
+        input(f"{separador}\nPress enter to return to the main menu ")
 
     if user == "2":
-        user = input("Introduzca un país: ").lower()
+        user = input("Name of the country: ").lower()
         country_to_search = cf.get_by_term(user)
         if country_to_search:
             flag_url = country_to_search[0]["flags"]["svg"]
@@ -40,12 +42,23 @@ while user != "q".lower():
             with open(f"./img/{country_to_search[0]['name']['common'].lower()}.svg", mode="wb") as file:
                 file.write(flag)
         else:
-            print(f"El país {user.capitalize()} no existe en nuestra base de datos")
-        print("-"*30)
-        input("Presione enter para volver al menú principal ")
+            print(f"The country {user.capitalize()} does not exist in our data base")
+        input(f"{separador}\nPress enter to return to the main menu ")
 
     if user == "3":
-        pass 
+        user = cf.choose_continent()
+        print(separador)
+        request = cf.get_by_term(user, continent=True)
         
-print("¡Hasta pronto!")
+        countries = list(map(lambda country: {
+            "name":country["name"]["common"],
+            # "capital":country["capital"][0],
+            # "capital":country.get("capital"[0]),
+            "area":country["area"],
+            "drive_side":country["car"]["side"],
+            "population":country["population"]
+        }, request))
 
+        cf.quiz(countries,user)
+
+        input(f"{separador}\nPress enter to return to the main menu ")

754aa21bfd23a6404c8ef53d365320ba3b2051b9
Araaancg <carpinteroaranchag@gmail.com>
2022-01-12 12:13:29 +0100

    terminado 100%

--- airport/main.py
+++ airport/main.py
@@ -1,15 +1,19 @@
 import datetime as dt
+from math import e
 from models import *
+from random import randint
+import json
 
-flights = get_data("./airports.json")
-# print(flights)
+airports = get_data("./airports.json")
 user = ""
 
-cities = ["Sao Paulo","Madrid","Lima","Buenos Aires"]
 
 separador = "-"*30
 
 while user.lower() != "q":
+    tickets = get_data("./tickets.json")
+    # print(tickets)
+    cities = [city["city"] for city in airports.values()]
     menu()
     user = input(": ")
     print(separador)
@@ -34,18 +38,19 @@ while user.lower() != "q":
         print(separador)
 
         #CHOOSE TIME OF DEPARTURE
-        departures = flights[country_codes[destination]][country_codes[origin]]["departures"]
+        departures = airports[country_codes[destination]][country_codes[origin]]["departures"]
 
         print("Choose a time of departure:")
         for i,time in enumerate(departures):
             print(f"{i+1}. {time}")
         departure_hour = departures[int(input(": ")) -1]
 
-        flight_time = flights[country_codes[destination]][country_codes[origin]]["flight_time"]
-        utc_destination = flights[country_codes[destination]]["UTC"]
+        flight_time = airports[country_codes[destination]][country_codes[origin]]["flight_time"]
+        utc_destination = airports[country_codes[destination]]["UTC"]
+        utc_origin = airports[country_codes[origin]]["UTC"]
 
         #CREATE OBJECT
-        flight_object = Flight(origin,destination,departure_hour,flight_time,utc_destination)
+        flight_object = Flight(origin,destination,departure_hour,flight_time,utc_destination,utc_origin,str(randint(100000,999999)))
         flight_object.write_data
 
         print(separador)
@@ -55,7 +60,65 @@ while user.lower() != "q":
         input(f"{separador}\nPress enter to return to the main menu ")
 
     if user == "2": #Cancel flight
-        pass
+        flight_id = input("Enter your flight's id: ")
+        print(separador)
+        all_flights_id = [ticket["id"] for ticket in tickets["tickets"]]
 
-    if user == "3": #Change flight
-        pass
+        if flight_id in all_flights_id:
+            print("FLIGHT TO CANCEL".center(30))
+            for k,v in tickets["tickets"][all_flights_id.index(flight_id)].items():
+                print(f"{k.capitalize()}: {v}")
+            print(separador)
+            user = input("Are you sure you want to cancel this flight? (y/n): ")
+            if user.lower() == "y":
+                tickets["tickets"].pop(all_flights_id.index(flight_id))
+                with open("./tickets.json", mode="w", encoding="utf8") as file:
+                    json.dump(tickets,file,ensure_ascii=False,indent=4)
+                print("Flight canceled")
+            else:
+                print("Action canceled")
+        
+        else:
+            print("Sorry, you have no flights with that id number")
+
+        input(f"{separador}\nPress enter to return to the main menu ")
+
+    if user == "3": #Change flight, solo se puede cambiar la hora de salida
+        flight_id = input("Enter your flight's id: ")
+        print(separador)
+        all_flights_id = [ticket["id"] for ticket in tickets["tickets"]]
+
+        if flight_id in all_flights_id:
+            print("FLIGHT TO MODIFY".center(30))
+            for k,v in tickets["tickets"][all_flights_id.index(flight_id)].items():
+                print(f"{k.capitalize()}: {v}")
+            print(separador)
+            
+            user = input("Are you sure you want to modify your departure hour? (y/n): ")
+            print(separador)
+            if user.lower() == "y":
+
+                origin, destination = tickets["tickets"][all_flights_id.index(flight_id)]["origin"], tickets["tickets"][all_flights_id.index(flight_id)]["destination"]
+
+                departures = airports[country_codes[destination]][country_codes[origin]]["departures"]
+                departures.remove(tickets["tickets"][all_flights_id.index(flight_id)]["departure_hour"])
+
+                print("Choose a time of departure:")
+                for i,time in enumerate(departures):
+                    print(f"{i+1}. {time}")
+                departure_hour = departures[int(input(": ")) -1]
+
+                print(separador)
+
+                tickets["tickets"][all_flights_id.index(flight_id)]["departure_hour"] = departure_hour
+                with open("./tickets.json", mode="w", encoding="utf8") as file:
+                    json.dump(tickets,file,ensure_ascii=False,indent=4)
+                print("Departure hour modified")
+            
+            else:
+                print("Action canceled")
+        
+        else:
+            print("Sorry, you have no flights with that id number")
+
+        input(f"{separador}\nPress enter to return to the main menu ")
--- airport/models.py
+++ airport/models.py
@@ -1,7 +1,6 @@
 import json
 import datetime as dt
-from hashlib import sha256
-from random import random
+
 
 SECRET = b"ryanground"
 
@@ -26,49 +25,37 @@ def get_data(json_file):
         return json.load(file)
 
 flights = get_data("./airports.json")
+tickets_dicc = get_data("tickets.json")
 
 class Flight:
-    def __init__(self, origin, destination, departure_hour, flight_time, utc_destination):
+    def __init__(self, origin, destination, departure_hour, flight_time, utc_destination,utc_origin,identification_number):
         self.origin = origin
         self.destination = destination
         self.departure_hour = departure_hour
         self.flight_time = flight_time
         self.utc_destination = utc_destination
+        self.utc_origin = utc_origin
+        self.identification_number = identification_number
         
-    # @staticmethod
-    # def tomorrow():
-    #     pre = dt.datetime.now() + dt.datetime(days=1)
-    #     pos = dt.datetime(year=pre.year, month=pre.month, day=pre.day)
-    #     return pos
+    @staticmethod
+    def tomorrow():
+        pre = dt.datetime.now() + dt.timedelta(days=1)
+        pos = dt.datetime(year=pre.year, month=pre.month, day=pre.day)
+        return pos
 
     @property
     def eta(self):
-        date1 = dt.datetime.fromisoformat(f"2022-01-07T{self.departure_hour}")
+        date1 = dt.datetime.fromisoformat(f"{dt.datetime.isoformat(self.tomorrow()).split('T')[0]}T{self.departure_hour}")
         flight_time = dt.timedelta(hours=self.flight_time)
         eta_origen = date1 + flight_time
-        utc_destination = dt.timedelta(hours=self.utc_destination)
+        utc_destination = dt.timedelta(hours=(self.utc_destination-self.utc_origin))
         eta_destination = eta_origen + utc_destination
         return eta_origen, eta_destination
-
-    def pretty_print(self):
-        print("FLIGHT INFORMATION".center(30))
-        print(f"Origin: {self.origin}")
-        print(f"Destination: {self.destination}")
-        print(f"F. Duration: {self.flight_time} hours")
-        print(f"Estimated arrival (origin): {self.eta[0]} hora en {self.origin}")
-        print(f"Estimated arrival (local): {self.eta[1]} hora en {self.destination}")
-    
-    @property
-    def create_id(self):
-        id = sha256(self.origin.encode())
-        id.update(self.destination.encode())
-        id.update(self.departure_hour.encode())
-        return id.hexdigest()
-
+  
     @property
     def flight_dicc(self):
         flight_dicc = {
-            "id":self.create_id,
+            "id":self.identification_number,
             "purchase_time":dt.datetime.isoformat(dt.datetime.now()),
             "origin":self.origin,
             "destination":self.destination,
@@ -79,11 +66,16 @@ class Flight:
 
     @property
     def write_data(self):
-        dicc = {"tickets":[]}
-        dicc["tickets"].append(self.flight_dicc)
+        tickets_dicc["tickets"].append(self.flight_dicc)
         with open("./tickets.json", mode="w", encoding="utf8") as file:
-            json.dump(dicc,file,ensure_ascii=False,indent=4)
-
-
-
+            json.dump(tickets_dicc,file,ensure_ascii=False,indent=4)
     
+    def pretty_print(self):
+        print("FLIGHT INFORMATION".center(30))
+        print(f"Identification number: {self.identification_number}")
+        print(f"Origin: {self.origin}")
+        print(f"Destination: {self.destination}")
+        print(f"Departure time: {self.departure_hour}")
+        print(f"F. Duration: {self.flight_time} hours")
+        print(f"Estimated arrival (origin): {self.eta[0]} hora en {self.origin}")
+        print(f"Estimated arrival (local): {self.eta[1]} hora en {self.destination}")

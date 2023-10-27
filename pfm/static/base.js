ae2069d50a8fae3a0f6ccb385678719c54f75f48
Araaancg <carpinteroaranchag@gmail.com>
2023-10-27 18:25:34 +0200

    change repo

--- pfm/static/base.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    if (window.localStorage.getItem("theme") === "light") {
-        const body = document.querySelector("body");
-        body.className = "light";
-    };
-};
